c1b7da1e06cc220ffb0780da99f97fc0c603d73c
DESKTOP-PMSV1UO\NOH EUNBI <nohonbi01@gmail.com>
2024-04-03 17:22:14 +0900

    main, about intro add

--- src/routes/About.js
+++ src/routes/About.js
@@ -7,34 +7,35 @@ function About() {
     <div className="about-container">
       <div className="introduce-box">
         <div className="left-area">
-          <span className="title"> Water Water Exchange ?</span>
-          <span > "물물교환"을 키워드로,</span>
-          <span > Item-Item 거래를 통해 버려지는 생활쓰레기를 줄이고</span>
-          <span > 사고팔기의 사이클 속에서 낭비되는 시간과 소비를 줄임으로써</span>
-          <span > ` Excercise to Saving Time & Property `</span>
-          <span > 물 흘러가듯 좋은 운동을 만들자는 중의적 뜻을 품고</span>
-          <span > 물건거래 사이트를 제작하게 되었습니다.</span>
-          
-          <span className="title-point"> Point </span>
-          <span>1. 물건의 가격이 아닌, 상대방의 수요가 교환물건의 가치를 결정</span>
-          <span>2. 교환되어 소유자가 바뀐 물건은 다시 재거래&기록 확인 가능</span>
-          <span>3. 댓글로 경매 시스템 유사 교환가치를 설득 or 협의 가능</span>
-          <span>4. 일상 포스팅을 통한 사용자 감성과 아이템 스토리 공유</span>
-          <span className="title-proceed"> Proceeding </span>
-          <span >-  거래완료 전 예약확정 상태로 1:1 채팅기능 제공 예정 </span>
-          <span >-  거래완료 후 직거래/택배/등 교환시스템 제외 </span>
+          <span className="title"> Who is Developer ?</span>
+          <span > <p>Name</p> Eunbi Noh </span>
+          <span > <p>Developer</p> F/E </span>
+          <span > <p>Technician</p> _ </span>
+          <span > <p>Career</p> _ </span>
+          <span > <p>Skills</p> _ </span>
+          <div>
+            <img src="/assets/img/FE_SPEC1.png" width="120px;"  height="60px;" />
+            <img src="/assets/img/FE_SPEC2.png" width="100px;"  height="60px;" />
+          </div>  
+          <div>
+            <img src="/assets/img/FE_SPEC3.png" width="50px;"  height="50px;" />
+            <img src="/assets/img/FE_SPEC4.png" width="50px;"  height="50px;" />
+            <img src="/assets/img/FE_SPEC5.png" width="50px;"  height="50px;" />
+          </div>
+
+          <button> 문의하기 </button>
         </div>
         <div className="right-area">
-          <span className="title"> Who is Developer ?</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
-          <span > Water Water Exchange 소개</span>
+          <span className="title"> Projects History</span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
+          <span > _ </span>
 
           <button> 문의하기 </button>
         </div>
