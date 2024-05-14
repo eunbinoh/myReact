e7a3b7f0862bd26f7b2f5b63302ee65cca0a4cd9
eunbinoh <eunbi112@naver.com>
2024-05-14 19:42:52 +0900

    remove proj

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './assets/style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, useLocation  } from 'react-router-dom'
 import Home from './routes/Home.js';
 
-import Project_1 from './views/Project_1.js'
-import Project_2 from './views/Project_2.js'
-import Project_3 from './views/Project_3.js'
-
 function App() {
-  const location = useLocation();
-  const { pathname } = location;
-
   return (
     <div className="app">
-      {
-        JSON.stringify({pathname}).indexOf('proj_') < 0 ? <Home /> : null
-      }
-       
-      <Routes>
-        <Route path="/proj_metahub" element={ <Project_1 /> } /> 
-        <Route path="/proj_metahub" element={ <Project_2 /> } />
-        <Route path="/proj_metahub" element={ <Project_3 /> } />
-      </Routes>
+      <Home />
     </div>
   );
 }
